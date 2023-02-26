1ecf1a144eef56a6133358e7b0fdb6033b666985
LAWSON <53177468+tonysson@users.noreply.github.com>
2023-02-26 16:49:51 +0100

    feat: third commit

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('*', (req, res) => {
-  res.send('hey');
+app.get("*", (req, res) => {
+  res.send("hey");
 });
 
 app.listen(8000, () => {
-  console.log('listening on port 8000');
+  console.log("listening on port 8000");
 });
